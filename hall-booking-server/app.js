ef5beb2bb5617195de7c5a64d77c8396c3904a71
hariharan-30 <hariharan300901@gmail.com>
2025-04-18 14:24:09 +0530

    new

--- hall-booking-server/app.js
+++ hall-booking-server/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { ObjectId, MongoClient } = require("mongodb");
 const cors = require("cors");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
 const yup = require("yup"); // Import yup for validation
